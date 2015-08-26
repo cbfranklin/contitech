9ad808763496a3271163d161e52f291a0b5dfdee
Connor Franklin <connorbfranklin@gmail.com>
2015-08-26 18:47:51 -0400

    add initial size logic for parallax sections, temp BGs for testing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,35 @@
 // jshint devel:true
+if (!contitech) {
+    var contitech = {}
+}
+
+$(function(){
+	contitech.init();
+})
+
+contitech.init = function(){
+	contitech.getWindowHeight();
+	$(window).on('resize',function(){
+		contitech.getWindowHeight();
+	})
+}
 
 //get screen height, set to refresh on resize
+contitech.getWindowHeight = function() {
+    console.log('getWindowHeight')
+    var height = $(window).height();
+    var width = $(window).width();
+    contitech.windowDimensions = {
+        width: width,
+        height: height
+    }
+    contitech.setSectionHeight()
+}
 
 //set section heights to screen height
-
-
+contitech.setSectionHeight = function() {
+    console.log('setSectionHeight')
+    $('section').css({
+        'height': contitech.windowDimensions.height
+    })
+}
