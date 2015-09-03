fa0971a3ceea397fc5e3a91e90269a74b7830bde
Connor <connorbfranklin@gmail.com>
2015-09-03 18:49:55 -0400

    sticky ability nearing completion, using WOW for "parallax" prefixes and content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,53 +11,76 @@ $(function() {
 //init
 contitech.init = function() {
     contitech.loadSections(content);
-    contitech.getWindowHeight();
+    contitech.getWindowDimensions();
     contitech.parallax();
     contitech.navigation();
+    contitech.getWindowDimensions();
+    contitech.setStickyAbility();
     $(window).on('resize', function() {
-        contitech.getWindowHeight();
+        contitech.getWindowDimensions();
+        contitech.setStickyAbility();
     })
     new WOW().init();
 }
 
 //get screen height, set to refresh on resize
-contitech.getWindowHeight = function() {
+contitech.getWindowDimensions = function() {
     var height = $(window).height();
     var width = $(window).width();
     contitech.windowDimensions = {
         width: width,
         height: height + 1
     }
-    contitech.setSectionHeight()
+    contitech.setSectionHeight();
+}
+
+contitech.setStickyAbility = function(){
+    $('#sticky-ability').css({
+        left: $('#discover-ability .ability-hidden').offset().left,
+        top: $('#discover-ability .ability-hidden').offset().top - $('#discover-ability').offset().top - 40
+    })
 }
 
 contitech.loadSections = function(content) {
-    var template = $('#templates .ability').html();
+
+    //sections
+    var template = $('#templates .template-ability').html();
     var rendered = Mustache.render(template, content);
     $('#discover-ability').after(rendered);
     $('.content-carousel').each(function() {
         $(this).find('.item').eq(0).addClass('active')
     })
+
+    //navigation
+    var template = $('#templates .nav-ability').html();
+    var rendered = Mustache.render(template, content);
+    $('#nav-ability').html(rendered);
+
     contitech.sections = [{
         el: 'add-more-ability',
-        nav: false
+        nav: false,
+        stickyAbility: false
     }, {
         el: 'discover-ability',
-        nav: false
+        nav: false,
+        stickyAbility: true
     }];
     for (var i in content) {
         contitech.sections.push({
             el: content[i].prefix + '-ability',
-            nav: true
+            nav: true,
+            stickyAbility: true
         })
     }
     contitech.sections.push({
         el: 'what-ability',
-        nav: false
+        nav: false,
+        stickyAbility: false
     })
     contitech.sections.push({
         el: 'contact',
-        nav: false
+        nav: false,
+        stickyAbility: false
     })
 }
 
@@ -72,7 +95,6 @@ contitech.setSectionHeight = function() {
 contitech.parallax = function() {
     contitech.parallax.controller = new ScrollMagic.Controller({
         globalSceneOptions: {
-            duration: '125%',
             tweenChanges: true
         }
     });
@@ -80,12 +102,15 @@ contitech.parallax = function() {
     $.each(contitech.sections, function(i, val) {
         var el = val.el;
         var nav = val.nav;
+        var stickyAbility = val.stickyAbility;
         var opacityTween = TweenMax.to('#' + el + ' .background', 0.6, {
             opacity: 1,
         });
         contitech.parallax.scene[el] = new ScrollMagic.Scene({
                 triggerElement: '#' + el,
-                triggerHook: "onEnter"
+                triggerHook: "onEnter",
+                duration: "125%",
+                offset: "10%"
             })
             .setTween("#" + el + " .background", {
                 y: "40%",
@@ -109,21 +134,39 @@ contitech.parallax = function() {
         } else {
             contitech.parallax.scene[el].on('enter', contitech.hideNavigation);
         }
+
+        if (stickyAbility) {
+            if(el === 'discover-ability'){
+                 contitech.parallax.scene[el].on('enter', contitech.showStickyAbility);
+            }
+            else{
+                contitech.parallax.scene[el].on('enter', contitech.showStickyAbility);
+            }
+        } else {
+            contitech.parallax.scene[el].on('enter', contitech.hideStickyAbility);
+        }
     })
-    contitech.parallax.scene['ability-pin'] = new ScrollMagic.Scene({
-            triggerElement: '#discover-ability em',
-            triggerHook: 'onCenter'
+    /*contitech.parallax.scene['ability-pin'] = new ScrollMagic.Scene({
+            triggerElement: '#discover-ability .ability',
+            offset: contitech.windowDimensions.height * 0.222,
+            duration: $('.sections').height(),
+            reverse: true
         })
-        .setPin('#discover-ability em')
+        .setPin('#discover-ability .ability')
         .addIndicators()
-        .addTo(contitech.parallax.controller);
+        .addTo(contitech.parallax.controller);*/
 
 }
-
+contitech.showStickyAbility = function(){
+    $('#sticky-ability').show()
+}
+contitech.hideStickyAbility = function(){
+    $('#sticky-ability').hide()
+}
 //ability navigation
 contitech.navigation = function() {
-        $('.ability-nav a').on('click', function(event) {
-            $('.ability-nav a').removeClass('active');
+        $('#nav-ability a').on('click', function(event) {
+            $('#nav-ability a').removeClass('active');
             $(this).addClass('active')
             var ability = $(this).attr('data-ability');
             $('html, body').animate({
@@ -131,16 +174,25 @@ contitech.navigation = function() {
             }, 1000);
             event.preventDefault();
         })
+        $('#scroll-for-more-ability').on('click',function(){
+             $('html, body').animate({
+                scrollTop: $('#discover-ability').offset().top + 20
+            }, 1000);
+        })
     }
     //auto-update navigation on scroll
 contitech.updateNavigation = function(e) {
-    $('.ability-nav').show();
+    $('#nav-ability').show();
     var ability = e.target.triggerElement().id.replace('-ability', '')
-    $('.ability-nav a').removeClass('active');
-    $('.ability-nav a[data-ability="' + ability + '"]').addClass('active')
+    $('#nav-ability a').removeClass('active');
+    $('#nav-ability a[data-ability="' + ability + '"]').addClass('active')
 }
 contitech.hideNavigation = function(e) {
-    $('.ability-nav').hide();
+    $('#nav-ability').hide();
+}
+
+contitech.resetWow = function(){
+    new WOW().init();
 }
 
 contitech.sections = []
